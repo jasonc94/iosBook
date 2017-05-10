b8bc6a86b38f45e66bf81c915be006b55c59c6c0
Jason Cai <jasoncai528@gmail.com>
2017-05-09 19:42:25 -0700

    Initial Commit

---
+++ XBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XBook
+//
+//  Created by Jason Cai on 5/9/17.
+//  Copyright © 2017 Jason Cai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
